7eae7e36add6cf7f3a22e3d68d2f89494cbcdb53
Chris Chrisostomou <dev@mou.me>
2015-11-24 20:38:17 +0000

    Added dark-cyan theme; fixed some spotify-dark theme issues

---
+++ radiant-player-mac/Styles/DarkCyanStyle.h
@@ -0,0 +1,14 @@
+/*
+ * DarkCyanStyle.h
+ *
+ * Created by Daniel Stewart & Chris Chrisostomou.
+ *
+ * Subject to terms and conditions in LICENSE.md.
+ *
+ */
+
+#import "ApplicationStyle.h"
+
+@interface DarkCyanStyle : ApplicationStyle
+
+@end
